63c45bf5f6385ee2120513deed78e5b2a3ff9def
periwalkeshav <keshav.periwal2018@vitstudent.ac.in>
2019-08-13 18:24:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
-/**
- * animation finction
- */
-var tl = new TimelineMax({onUpdate:updatePercentage});
-var tl2 = new TimelineMax();
-const controller = new ScrollMagic.Controller();
+// /**
+//  * animation finction
+//  */
+// var tl = new TimelineMax({onUpdate:updatePercentage});
+// var tl2 = new TimelineMax();
+// const controller = new ScrollMagic.Controller();
 
-// tl.from('#des', 0.1, {x:-200, opacity: 0});
-tl.from('#des1', 0.1, {x:200, opacity: 0});
-tl.from('.container-fluid1', 0.1, {x:-200, opacity: 0});
-const scene = new ScrollMagic.Scene({
-  triggerElement: "#desc",
-            triggerHook: "onLeave",
-            duration: "100%"
-})
-  .setPin("#des")
-  .setTween(tl)
-		.addTo(controller);
-function updatePercentage() {
-      tl.progress();
-      console.log(tl.progress());
-    }
+// // tl.from('#des', 0.1, {x:-200, opacity: 0});
+// tl.from('#des1', 0.1, {x:200, opacity: 0});
+// tl.from('.container-fluid1', 0.1, {x:-200, opacity: 0});
+// const scene = new ScrollMagic.Scene({
+//   triggerElement: "#desc",
+//             triggerHook: "onLeave",
+//             duration: "100%"
+// })
+//   .setPin("#des")
+//   .setTween(tl)
+// 		.addTo(controller);
+// function updatePercentage() {
+//       tl.progress();
+//       console.log(tl.progress());
+//     }
 /*author bhavesh Function COuntdown Start */
 function countDown()
 {
